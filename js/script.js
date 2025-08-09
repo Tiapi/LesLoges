5631b40e721a60db1dee36e29fd4705df0e9f059
Mathys Mauron <mathysmauron@gmail.com>
2025-08-09 18:43:02 +0200

    fix script

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ document.addEventListener("DOMContentLoaded", () => {
         mainContent.scrollIntoView({ behavior: "smooth" });
     });
 });
+
+const hero = document.querySelector(".hero");
+
+window.addEventListener(
+    "wheel",
+    (event) => {
+        const scrollY = window.scrollY || window.pageYOffset;
+
+        if (scrollY <= 0 && event.deltaY < 0) {
+            // En haut de la page ET scroll vers le haut => bloquer
+            event.preventDefault();
+        }
+    },
+    { passive: false }
+);
